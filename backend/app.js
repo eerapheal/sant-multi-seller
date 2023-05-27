fde3e41ce050bdcd56a25d9188190fc21cbee66e
Ekpenisi Erue Raphael <ekpenisiraphael@gmail.com>
2023-05-27 22:47:09 +0530

    app.js

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,20 @@
-const express = require('express');
+const express = require("express");
+const ErrorHandler = require("./utils/ErrorHandler");
 const app = express();
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const fileUpload = require("express-fileupload")
+app.use(express.json());
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(fileUpload({useTempfiles: true}));
 
-
-if(process.env.NODE_ENV !== 'PRODUCTION') {
-    require('dotenv').config({
-        path:'backend/config/.env'
-    })
+if (process.env.NODE_ENV !== "PRODUCTION") {
+  require("dotenv").config({
+    path: "backend/config/.env",
+  });
 }
 
-module.exports =app;
+// ErrorHandling
+app.use(ErrorHandler);
+module.exports = app;
