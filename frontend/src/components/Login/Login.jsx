9bb0ad00a402967ab25f3f4f29e099346e650418
Ekpenisi Erue Raphael <ekpenisiraphael@gmail.com>
2023-05-27 06:56:24 +0530

    Login basic implementation

---
+++ frontend/src/components/Login/Login.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import LoginPage  from '../../pages/LoginPage';
+
+const Login = () => {
+  return (
+<div className='w-full h-screen bg-gray-50'>
+<LoginPage />
+</div>
+      
+   
+  )
+}
+
+export default Login
