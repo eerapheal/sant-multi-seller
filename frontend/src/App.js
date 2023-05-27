8017b74762dd8da09a5083d8d2b3496cad96365a
Ekpenisi Erue Raphael <ekpenisiraphael@gmail.com>
2023-05-27 06:54:52 +0530

    Routes added

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Login, SignUp } from "./Routes.js";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/sign-up" element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
